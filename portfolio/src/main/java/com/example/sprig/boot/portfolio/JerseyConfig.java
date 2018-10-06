e6ee5c9d2248acceaf8487497b766878b9419528
dkowzan <dkowzan@gmail.com>
2018-10-06 13:41:49 +0200

    demo finished

---
+++ portfolio/src/main/java/com/example/sprig/boot/portfolio/JerseyConfig.java
@@ -0,0 +1,14 @@
+package com.example.sprig.boot.portfolio;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+import javax.ws.rs.ApplicationPath;
+
+@Configuration
+@ApplicationPath("portfolios")
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+        register(PortfolioImpl.class);
+    }
+}
